f222d53cf3c8104887915e34633d1f04dfade0c2
Thomas - Laptop <thomas.elsen@rivy.org>
2024-11-19 16:17:06 +0100

    first version of custom_component

---
+++ custom_components/esphome_fake_dlbm/__init__.py
@@ -0,0 +1,28 @@
+"""
+The "dynamic_load_balancing" custom component.
+
+This component implements the bare minimum that a component should implement.
+
+Configuration:
+
+To use the dynamic_load_balancing component you will need to add the following to your
+configuration.yaml file.
+
+fake_dlbm:
+"""
+from __future__ import annotations
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+# The domain of your component. Should be equal to the name of your component.
+DOMAIN = "dynamic_load_balancing"
+
+
+def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up a skeleton component."""
+    # States are in the format DOMAIN.OBJECT_ID.
+    hass.states.set('dynamic_load_balancing.Hello_World', 'Works!')
+
+    # Return boolean to indicate that initialization was successfully.
+    return True
