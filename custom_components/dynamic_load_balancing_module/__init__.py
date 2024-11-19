9f523c5118c959e97c8c0ea9eca80eb6a6cd7010
Thomas - Laptop <thomas.elsen@rivy.org>
2024-11-19 16:30:14 +0100

    rename module to dynamic_load_balancing_module

--- custom_components/dynamic_load_balancing_module/__init__.py
+++ custom_components/dynamic_load_balancing_module/__init__.py
@@ -1,14 +1,14 @@
 """
-The "dynamic_load_balancing" custom component.
+The "dynamic_load_balancing_module" custom component.
 
 This component implements the bare minimum that a component should implement.
 
 Configuration:
 
-To use the dynamic_load_balancing component you will need to add the following to your
+To use the dynamic_load_balancing_module component you will need to add the following to your
 configuration.yaml file.
 
-fake_dlbm:
+dynamic_load_balancing_module:
 """
 from __future__ import annotations
 
@@ -16,13 +16,13 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
 # The domain of your component. Should be equal to the name of your component.
-DOMAIN = "dynamic_load_balancing"
+DOMAIN = "dynamic_load_balancing_module"
 
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up a skeleton component."""
     # States are in the format DOMAIN.OBJECT_ID.
-    hass.states.set('dynamic_load_balancing.Hello_World', 'Works!')
+    hass.states.set('dynamic_load_balancing_module.Hello_World', 'Works!')
 
     # Return boolean to indicate that initialization was successfully.
     return True
